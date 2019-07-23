e2409bbdceade71a44057c18436fdf834fe07689
tiru <tirub.1930@gmail.com>
2019-07-23 14:14:30 +0530

    finetuned for improving accuracy

--- Train.py
+++ Train.py
@@ -100,31 +100,32 @@ def train(opt):
         preds = crnn(image)
         preds_size = Variable(torch.IntTensor([preds.size(0)] * batch_size))
         cost = criterion(preds, text, preds_size, length) / batch_size
-        print(cost)
+        # print(cost)
         crnn.zero_grad()
         cost.backward()
         optimizer.step()
         return cost
 
-    # for epoch in range(opt.nepoch):
-    #     train_iter = iter(train_loader)
-    #     i = 0
-    #     while i < len(train_loader):
-    #         for p in crnn.parameters():
-    #             p.requires_grad = True
-    #         crnn.train()
-    #         cost = trainBatch(crnn, criterion, optimizer)
-    #         # print(cost)
-    #         loss_avg.add(cost)
-    #         i += 1
-    # torch.save(crnn.state_dict(), '{0}/netCRNN_{1}.pth'.format(opt.expr_dir, epoch))
+    for epoch in range(opt.nepoch):
+        train_iter = iter(train_loader)
+        i = 0
+        while i < len(train_loader):
+            for p in crnn.parameters():
+                p.requires_grad = True
+            crnn.train()
+            cost = trainBatch(crnn, criterion, optimizer)
+            # print(cost)
+            loss_avg.add(cost)
+            i += 1
+            # print(loss_avg)
+    torch.save(crnn.state_dict(), '{0}/netCRNN_{1}.pth'.format(opt.expr_dir, epoch))
 
     def val(net, dataset, criterion, max_iter=100):
         print('Start val')
 
-        if torch.cuda.is_available():
-            crnn = torch.nn.DataParallel(net).cuda()
-        crnn.load_state_dict(torch.load("./expr/netCRNN_99.pth"))
+        # if torch.cuda.is_available():
+        #     crnn = torch.nn.DataParallel(net).cuda()
+        # crnn.load_state_dict(torch.load("./expr/netCRNN_999.pth"))
 
         for p in crnn.parameters():
             p.requires_grad = False
@@ -132,7 +133,7 @@ def train(opt):
         crnn.eval()
         data_loader = torch.utils.data.DataLoader(
             dataset, shuffle=True, batch_size=opt.batchSize, num_workers=int(opt.workers))
-        
+        val_iter = iter(data_loader)
 
         i = 0
         n_correct = 0
@@ -140,9 +141,8 @@ def train(opt):
 
         max_iter = min(max_iter, len(data_loader))
         for i in range(max_iter):
-            val_iter = iter(data_loader)
             data = val_iter.next()
-            i += 1
+            # i += 1
             cpu_images, cpu_texts = data
             batch_size = cpu_images.size(0)
             utils.loadData(image, cpu_images)
@@ -166,6 +166,8 @@ def train(opt):
         for raw_pred, pred, gt in zip(raw_preds, sim_preds, cpu_texts):
             print('%-20s => %-20s, groundTruth: %-20s' % (raw_pred, pred, gt))
 
+        print(n_correct)
+        print(max_iter * opt.batchSize)
         accuracy = n_correct / float(max_iter * opt.batchSize)
         print('Test loss: %f, accuray: %f' % (loss_avg.val(), accuracy))
 
@@ -183,7 +185,7 @@ if __name__ == '__main__':
     parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
     parser.add_argument('--imgH', type=int, default=32, help='the height of the input image to network')
     parser.add_argument('--imgW', type=int, default=100, help='the width of the input image to network')
-    parser.add_argument('--nh', type=int, default=256, help='size of the lstm hidden state')
+    parser.add_argument('--nh', type=int, default=250, help='size of the lstm hidden state')
     parser.add_argument('--nepoch', type=int, default=100, help='number of epochs to train for')
     parser.add_argument('--cuda', action='store_true', help='enables cuda')
     parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
--- models/crnn.py
+++ models/crnn.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class BidirectionalLSTM(nn.Module):
 
@@ -22,18 +22,18 @@ class BidirectionalLSTM(nn.Module):
 
 class CRNN(nn.Module):
 
-    def __init__(self, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False):
+    def __init__(self, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=True):
         super(CRNN, self).__init__()
         assert imgH % 16 == 0, 'imgH has to be a multiple of 16'
 
         ks = [3, 3, 3, 3, 3, 3, 2]
         ps = [1, 1, 1, 1, 1, 1, 0]
-        ss = [1, 1, 1, 1, 1, 1, 1]
+        ss = [1, 1, 1, 1, 1, 1, 1,]
         nm = [64, 128, 256, 256, 512, 512, 512]
 
         cnn = nn.Sequential()
 
-        def convRelu(i, batchNormalization=False):
+        def convRelu(i, batchNormalization=True):
             nIn = nc if i == 0 else nm[i - 1]
             nOut = nm[i]
             cnn.add_module('conv{0}'.format(i),
@@ -59,6 +59,12 @@ class CRNN(nn.Module):
         cnn.add_module('pooling{0}'.format(3),
                        nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 512x2x16
         convRelu(6, True)  # 512x1x16
+        # cnn.add_module('pooling{0}'.format(3),
+        #                nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 512x2x16
+        # convRelu(7, True) 
+        # cnn.add_module('pooling{0}'.format(3),
+        #                nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 512x2x16
+        # convRelu(8, True) 
 
         self.cnn = cnn
         self.rnn = nn.Sequential(
@@ -67,6 +73,7 @@ class CRNN(nn.Module):
 
     def forward(self, input):
         # conv features
+        # conv_in = self.fe(input)
         conv = self.cnn(input)
         b, c, h, w = conv.size()
         assert h == 1, "the height of conv must be 1"
@@ -77,3 +84,29 @@ class CRNN(nn.Module):
         output = self.rnn(conv)
 
         return output
+
+
+class VGG_FeatureExtractor(nn.Module):
+    """ FeatureExtractor of CRNN (https://arxiv.org/pdf/1507.05717.pdf) """
+
+    def __init__(self, input_channel=1, output_channel=512):
+        super(VGG_FeatureExtractor, self).__init__()
+        self.output_channel = [int(output_channel / 8), int(output_channel / 4),
+                               int(output_channel / 2), output_channel]  # [64, 128, 256, 512]
+        self.ConvNet = nn.Sequential(
+            nn.Conv2d(input_channel, self.output_channel[0], 3, 1, 1), nn.ReLU(True),
+            nn.MaxPool2d(2, 2),  # 64x16x50
+            nn.Conv2d(self.output_channel[0], self.output_channel[1], 3, 1, 1), nn.ReLU(True),
+            nn.MaxPool2d(2, 2),  # 128x8x25
+            nn.Conv2d(self.output_channel[1], self.output_channel[2], 3, 1, 1), nn.ReLU(True),  # 256x8x25
+            nn.Conv2d(self.output_channel[2], self.output_channel[2], 3, 1, 1), nn.ReLU(True),
+            nn.MaxPool2d((2, 1), (2, 1)),  # 256x4x25
+            nn.Conv2d(self.output_channel[2], self.output_channel[3], 3, 1, 1, bias=False),
+            nn.BatchNorm2d(self.output_channel[3]), nn.ReLU(True),  # 512x4x25
+            nn.Conv2d(self.output_channel[3], self.output_channel[3], 3, 1, 1, bias=False),
+            nn.BatchNorm2d(self.output_channel[3]), nn.ReLU(True),
+            nn.MaxPool2d((2, 1), (2, 1)),  # 512x2x25
+            nn.Conv2d(self.output_channel[3], self.output_channel[3], 2, 1, 0), nn.ReLU(True))  # 512x1x24
+
+    def forward(self, input):
+        return self.ConvNet(input)
